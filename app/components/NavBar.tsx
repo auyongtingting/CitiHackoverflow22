ba1e2e7de72bb7570a0c1781f9ea68a0b2aa53b1
Au Yong Ting Ting <tingting.a@affinidi.com>
2022-08-11 15:50:06 +0800

    Logout Feature

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -140,7 +140,7 @@ export default function NavBar() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="/logout"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
